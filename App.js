0d2101522c9a4c3adce93707523a69befeac26c9
Andy Hubert <andy.hubert+8@gmail.com>
2017-10-19 00:37:32 +0300

    Code structure todo's

--- App.js
+++ App.js
@@ -1,16 +1,34 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Root } from "native-base";
 
 export default class App extends React.Component {
+  async componentWillMount() {
+    await Expo.Font.loadAsync({
+      'Roboto': require('native-base/Fonts/Roboto.ttf'),
+      'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+    });
+  }
+  
   render() {
     return (
-      <View style={styles.container}>
-        <Text>react-intl</Text>
-        <Text>redux + react-redux + redux-persist</Text>
-        <Text>react-native-elements</Text>
-        <Text>clear code structure</Text>
-        <Text>development cycle</Text>
-      </View>
+      <Root>
+        <View style={styles.container}>
+          <Text>native-base</Text>
+          <Text>react-navigation</Text>
+          <Text>redux + react-redux + redux-persist</Text>
+          <Text>react-intl</Text>
+          <Text>Downloading and saving books</Text>
+          <Text>
+            Shared stuff:
+              Data fetches and saving (redux + redux-persist)
+              Progress bar data
+              Search query
+              Footnote extraction
+          </Text>
+          <Text>development cycle</Text>
+        </View>
+      </Root>
     );
   }
 }
