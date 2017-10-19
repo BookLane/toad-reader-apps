f2d8b4b73c89fd394f23027dfa2636a7a60f43b3
Andy Hubert <andy.hubert+8@gmail.com>
2017-10-19 17:33:42 +0300

    added redux-persist

--- App.js
+++ App.js
@@ -2,38 +2,53 @@ import React from 'react'
 import Expo from "expo";
 import { Root } from "native-base"
 
+import { AsyncStorage } from 'react-native'
+import { createStore, compose } from 'redux'
+import { persistStore, autoRehydrate } from 'redux-persist'
 import allReducers from './src/reducers/all.js'
-import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 
 import GlobalNavigator from './src/navigators/Global.js'
 
-const store = createStore(allReducers)
+const store = compose(autoRehydrate())(createStore)(allReducers)
 
 export default class App extends React.Component {
   constructor() {
-    super();
+    super()
     this.state = {
-      isReady: false
+      isReady: false,
     };
   }
 
   async componentWillMount() {
+    const numPrepItems = 0
+
+    let prepCount = 0
+    const prepActionComplete = () => {
+      if(++prepCount >= numPrepItems) {
+        this.setState({ isReady: true })
+      }
+    }
+
+    persistStore(store, {storage: AsyncStorage}, prepActionComplete)
+        
     await Expo.Font.loadAsync({
       Roboto: require('native-base/Fonts/Roboto.ttf'),
       Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
       Ionicons: require("native-base/Fonts/Ionicons.ttf"),
     })
-    this.setState({ isReady: true });
+    prepActionComplete()
   }
   
   render() {
+
     if (!this.state.isReady) {
-      return <Expo.AppLoading />;
+      return <Expo.AppLoading />
     }
+
     return (
       <Root>
-        <Provider store= {store}>
+        <Provider store={store}>
           <GlobalNavigator />
         </Provider>
       </Root>
