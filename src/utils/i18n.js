0a11c4a3fd24248ba0d4d8ad10a1ae179a971192
Andy Hubert <andy.hubert+8@gmail.com>
2017-12-04 14:13:53 +0200

    Added translation description param

--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -1,24 +1,36 @@
 /*
 
-  Assumptions: An app instance will only be in a single language. 
+  Assumptions:
+    * An app instance will only be in a single language.
+    * Most of the time, the exact same English text should be translated the same. 
 
   Concept: As this code will be reused often to spin off new apps, make it easy to "flip a switch" and choose the language.
 
   ** English is written inline with i18n wrapper around it.
   ** Language is chosen simply via the import line below.
   ** Translations get their own file in the translations directory.
-  ** TODO: Write a script to build base translation files. Go through all files and find the i18n function
-  instances, making each first parameter of this function into a key in the translation file.
+  ** TODO: Write a script to build base translation files.
 
-  Examples:
+  Usage examples:
     i18n("Library")
     i18n("{{num_results}} results!", { num_results: this.props.numResults })
+    i18n("Back", {}, "i.e. go back")
+    i18n("Back", {}, "i.e. the back of the book")  // Differing descriptions create separate variables to translate
 
 */
 
 // import translations from "./translations/he.js"
 const translations = {}  // for English
 
-const i18n = (str, swaps={}) => (translations[str] || str).replace(/{{([^}]+)}}/g, (x, swapSpot) => swaps[swapSpot] || "")
+const i18n = (str, swaps={}, desc) => 
+  (
+    translations[str]!==undefined
+      ? (
+        translations[str][desc]!==undefined
+          ? translations[str][desc]
+          : translations[str]
+      )
+      : str
+  ).replace(/{{([^}]+)}}/g, (x, swapSpot) => swaps[swapSpot] || "")
 
 export default i18n
--- src/utils/translations/he.js
+++ src/utils/translations/he.js
@@ -1,4 +1,8 @@
 export default {
   "Library here!": "ספריה",
   "Library {{here}}!": "ספריה {{here}}",
+  "Back": {
+    "i.e. go back": "חזור",
+    "i.e. the back of the book": "גב",
+  },
 }
