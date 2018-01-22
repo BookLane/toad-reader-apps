da7ec5da086d0298d18213c956db9ec29fa75aab
Andy Hubert <andy.hubert+8@gmail.com>
2018-01-22 15:32:59 +0200

    Basic zoom in working

--- src/components/major/ZoomPage.js
+++ src/components/major/ZoomPage.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { FileSystem } from "expo"
-import { Animated, StyleSheet, Dimensions, StatusBar } from "react-native"
+import { Animated, Easing, StyleSheet, Dimensions, StatusBar, PixelRatio } from "react-native"
 
 import { getPageSize } from '../../utils/toolbox.js'
 
@@ -12,20 +12,24 @@ const styles = StyleSheet.create({
   snapshot: {
     position: 'absolute',
     backgroundColor: 'white',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
   },
 })
 
-const getZoomInFactor = () => {
+const getZoomOutScale = () => {
   const { width } = Dimensions.get('window')
   const { pageWidth } = getPageSize()
 
-  return width / pageWidth
+  return pageWidth / width
 }
 
 class ZoomPage extends React.Component {
 
   state = {
-    zoomFactor: new Animated.Value(this.props.zoomed ? getZoomInFactor() : 1),
+    scale: new Animated.Value(this.props.zoomed ? 1 : getZoomOutScale()),
   }
 
   componentWillReceiveProps(nextProps) {
@@ -38,12 +42,12 @@ class ZoomPage extends React.Component {
 
   animate = nextProps => {
     const { zoomed } = nextProps || this.props
-    const { zoomFactor } = this.state
+    const { scale } = this.state
 
     Animated.timing(
-      zoomFactor,
+      scale,
       {
-        toValue: zoomed ? getZoomInFactor() : 1,
+        toValue: zoomed ? 1 : getZoomOutScale(),
         duration: PAGE_ZOOM_MILLISECONDS,
       }
     ).start()
@@ -51,7 +55,7 @@ class ZoomPage extends React.Component {
 
   render() {
     const { bookId, spineIdRef, pageIndexInSpine, snapshotCoords, zoomed } = this.props
-    const { zoomFactor } = this.state
+    const { scale } = this.state
 
     const { width, height } = Dimensions.get('window')
     const uri = `${FileSystem.documentDirectory}snapshots/${bookId}/${spineIdRef}_${pageIndexInSpine}_${width}x${height}.jpg`
@@ -59,18 +63,30 @@ class ZoomPage extends React.Component {
     const { pageWidth, pageHeight } = getPageSize()
     const left = (snapshotCoords && snapshotCoords.x) || width - pageWidth/2
     const top = ((snapshotCoords && snapshotCoords.y) || height - pageHeight/2) + (StatusBar.currentHeight || 0)
+
+    const zoomOutScale = getZoomOutScale()
+
+    const translateX = scale.interpolate({
+      inputRange: [zoomOutScale, 1],
+      outputRange: [PixelRatio.getPixelSizeForLayoutSize(left - (width/2 - pageWidth/2)), 0],
+    })
+
+    const translateY = scale.interpolate({
+      inputRange: [zoomOutScale, 1],
+      outputRange: [PixelRatio.getPixelSizeForLayoutSize(top - (height/2 - pageHeight/2)), 0],
+    })
+    
     
     const zoomStyles = {
-      top,
-      left,
-      width: pageWidth,
-      height: pageHeight,
       transform: [
         {
-          scaleX: zoomFactor,
+          scale,
         },
         {
-          scaleY: zoomFactor,
+          translateX,
+        },
+        {
+          translateY,
         },
       ],
     }
