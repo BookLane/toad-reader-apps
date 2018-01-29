787a8c5b37c9a9a4937182962a44a6a7f6060629
Andy Hubert <andy.hubert+8@gmail.com>
2018-01-29 08:45:51 +0200

    Take the snapshot twice if first appears blank

--- src/utils/takeSnapshot.js
+++ src/utils/takeSnapshot.js
@@ -2,13 +2,28 @@ import { FileSystem, takeSnapshotAsync } from "expo"
 
 export default async ({ view, uri, width, height }) => {
 
-  const initFileURI = await takeSnapshotAsync(view, {
-    format: "jpg",
-    quality: 0.8,
-    result: "file",
-    width,
-    height,
-  })
+  await new Promise(resolve => setTimeout(resolve, 20))  // without this, I often get a blank image
+
+  const getSnapshot = async () => (
+    await takeSnapshotAsync(view, {
+      format: "jpg",
+      quality: 0.8,
+      result: "file",
+      width,
+      height,
+    })
+  )
+  
+  let initFileURI = await getSnapshot()
+
+  const fileInfo = await FileSystem.getInfoAsync(initFileURI)
+  if(fileInfo.size < 2500) {
+    // May have captured a blank page. Try again.
+    console.log('Taking snapshot again as first snapshot appears blank.', fileInfo.size, uri)
+    initFileURI = await getSnapshot()
+    const newFileInfo = await FileSystem.getInfoAsync(initFileURI)
+    console.log('New snapshot size: ', newFileInfo.size)
+  }
 
   const dir = uri.replace(/[^/]+$/, '')
   
