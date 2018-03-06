a9e3b16db8759e448cda3175a5bb7635e9436fdd
Andy Hubert <andy.hubert+8@gmail.com>
2018-03-06 11:49:24 +0200

    simplified updateReader with AsyncStorage

--- src/utils/updateReader.js
+++ src/utils/updateReader.js
@@ -1,43 +1,34 @@
+import { AsyncStorage } from "react-native"
 import { FileSystem } from "expo"
-
-import downloadAsync from "./downloadAsync.js"
 
 import { fetchZipAndAssets } from "./zipDownloader.js"
 
+// the READER_VERSION_TIMESTAMP needs to be updated any time any reader files are updated
 const {
   READER_VERSION_TIMESTAMP,
 } = Expo.Constants.manifest.extra
 
-// configuration constants
-// the READER_VERSION_TIMESTAMP needs to be updated any time any reader files are updated
-const zipUrl = `https://s3-us-west-2.amazonaws.com/biblemesh-readium/cloud-reader-lite/${READER_VERSION_TIMESTAMP}/reader.zip`
-
 const updateReader = async () => {
   
   console.log(`Check reader...`)
-  
-  const readerDir = `${FileSystem.documentDirectory}reader`
-  const currentVersionUri = `${readerDir}/currentReaderVersion.txt`
 
-  const readerDirInfo = await FileSystem.getInfoAsync(readerDir)
-
-  if(readerDirInfo.exists) {
-    const currentVersionInfo = await FileSystem.getInfoAsync(currentVersionUri)
-    const versionTimestampOfCurrentReader = currentVersionInfo.exists ? await FileSystem.readAsStringAsync(currentVersionUri) : null
-    if(versionTimestampOfCurrentReader == READER_VERSION_TIMESTAMP) {
-      console.log(`Reader up-to-date.`)
-      return
-    }
+  const versionTimestampOfCurrentReader = await AsyncStorage.getItem('readerVersionTimestamp')
+  if(versionTimestampOfCurrentReader == READER_VERSION_TIMESTAMP) {
+    console.log(`Reader up-to-date.`)
+    return
   }
 
   console.log(`Download updated reader...`)
+  
+  const zipUrl = `https://s3-us-west-2.amazonaws.com/biblemesh-readium/cloud-reader-lite/${READER_VERSION_TIMESTAMP}/reader.zip`
+  const localBaseUri = `${FileSystem.documentDirectory}reader/`
 
   await fetchZipAndAssets({
     zipUrl,
-    localBaseUri: `${readerDir}/`,
+    localBaseUri,
   })
 
-  await FileSystem.writeAsStringAsync(currentVersionUri, READER_VERSION_TIMESTAMP)
+  await AsyncStorage.setItem('readerVersionTimestamp', READER_VERSION_TIMESTAMP)
 
   console.log(`Done downloading reader.`)
         
