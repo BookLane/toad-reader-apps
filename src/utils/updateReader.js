6f63ceda82d59fbf1a0d44dc7c410fced991f058
Andy Hubert <andy.hubert+8@gmail.com>
2018-03-06 10:44:00 +0200

    use zipDownloader to download reader

--- src/utils/updateReader.js
+++ src/utils/updateReader.js
@@ -2,57 +2,38 @@ import { FileSystem } from "expo"
 
 import downloadAsync from "./downloadAsync.js"
 
+import { fetchZipAndAssets } from "./zipDownloader.js"
+
 // configuration constants
-const readerAssetsLastUpdatedDate = `2018-03-06`  // this needs to be updated any time any reader files are updated
-const readerAssetsRemoteURL = `https://s3-us-west-2.amazonaws.com/biblemesh-readium-dev/cloud-reader-lite/`
-const readerAssetFiles = [
-  'index.html',
-  'css/annotations.css',
-  'css/readium-all.css',
-  'fonts/glyphicons-halflings-regular.eot',
-  'fonts/glyphicons-halflings-regular.svg',
-  'fonts/glyphicons-halflings-regular.ttf',
-  'fonts/glyphicons-halflings-regular.woff',
-  'fonts/glyphicons-halflings-regular.woff2',
-  'scripts/readium-js-viewer_all_LITE.js',
-  'scripts/mathjax/MathJax.js',
-  'scripts/zip/deflate.js',
-  'scripts/zip/inflate.js',
-  'scripts/zip/z-worker.js',
-]
+const readerTimestamp = `1520325374`  // this needs to be updated any time any reader files are updated
+const zipUrl = `https://s3-us-west-2.amazonaws.com/biblemesh-readium/cloud-reader-lite/${readerTimestamp}/reader.zip`
 
 const updateReader = async () => {
   
   console.log(`Check reader...`)
   
   const readerDir = `${FileSystem.documentDirectory}reader`
-  const lastUpdateDateUri = `${readerDir}/lastUpdateDate.txt`
+  const currentTimestampUri = `${readerDir}/currentReaderTimestamp.txt`
 
   const readerDirInfo = await FileSystem.getInfoAsync(readerDir)
 
   if(readerDirInfo.exists) {
-    const lastUpdateDateUriInfo = await FileSystem.getInfoAsync(lastUpdateDateUri)
-    const lastUpdateDateOfCurrentReader = lastUpdateDateUriInfo.exists ? await FileSystem.readAsStringAsync(lastUpdateDateUri) : null
-    if(lastUpdateDateOfCurrentReader == readerAssetsLastUpdatedDate) {
+    const currentTimestampInfo = await FileSystem.getInfoAsync(currentTimestampUri)
+    const timestampOfCurrentReader = currentTimestampInfo.exists ? await FileSystem.readAsStringAsync(currentTimestampUri) : null
+    if(timestampOfCurrentReader == readerTimestamp) {
       console.log(`Reader up-to-date.`)
       return
     }
   }
 
   console.log(`Download updated reader...`)
-  
-  await FileSystem.deleteAsync(readerDir, { idempotent: true })
 
-  await Promise.all(
-    readerAssetFiles.map(readerAssetFile => (
-      downloadAsync(
-        `${readerAssetsRemoteURL}${readerAssetFile}`,
-        `${readerDir}/${readerAssetFile}`
-      )
-    ))
-  )
+  await fetchZipAndAssets({
+    zipUrl,
+    localBaseUri: `${readerDir}/`,
+  })
 
-  await FileSystem.writeAsStringAsync(lastUpdateDateUri, readerAssetsLastUpdatedDate)
+  await FileSystem.writeAsStringAsync(currentTimestampUri, readerTimestamp)
 
   console.log(`Done downloading reader.`)
         
