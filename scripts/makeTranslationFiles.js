dda903a2c683f4de6b4cbd877073640fa7b8738c
Andy Hubert <andy.hubert+8@gmail.com>
2018-05-26 21:49:20 +0300

    Completed make translation files script

--- scripts/makeTranslationFiles.js
+++ scripts/makeTranslationFiles.js
@@ -3,11 +3,11 @@
   Example json translation file:
 
     {
-      "Library here!": "TRANSLATE NEEDED",
-      "Library {{here}}!": "TRANSLATE NEEDED",
+      "Library here!": "TRANSLATION NEEDED",
+      "Library {{here}}!": "TRANSLATION NEEDED",
       "Back": {
-        "i.e. go back": "TRANSLATE NEEDED",
-        "i.e. the back of the book": "TRANSLATE NEEDED"
+        "i.e. go back": "TRANSLATION NEEDED",
+        "i.e. the back of the book": "TRANSLATION NEEDED"
       }
     }
 
@@ -18,10 +18,14 @@
 const findInFiles = require('find-in-files')
 const fs = require('fs')
 
+const TRANSLATION_NEEDED = "TRANSLATION NEEDED"
+
 console.log('')
 console.log('Preparing to create translations needed files...')
+console.log('')
 
-let numFilesCreated = 0
+const langsGettingNewFile = []
+const langsNotGettingNewFile = []
 let defaultTranslationObj = {}
 
 // go through all files in src and extract calls to i18n(), building out the default json
@@ -44,9 +48,9 @@ findInFiles.find(`i18n\\((?:[^)"']*|"[^"]*"|'[^']*')*\\)`, 'src', '.js$')
 
         if(parts[2] === undefined) {
           if(typeof defaultTranslationObj[engText] === 'object') {
-            defaultTranslationObj[engText][""] = "TRANSLATE NEEDED"
+            defaultTranslationObj[engText][""] = TRANSLATION_NEEDED
           } else {
-            defaultTranslationObj[engText] = "TRANSLATE NEEDED"
+            defaultTranslationObj[engText] = TRANSLATION_NEEDED
           }
 
         } else {
@@ -58,22 +62,20 @@ findInFiles.find(`i18n\\((?:[^)"']*|"[^"]*"|'[^']*')*\\)`, 'src', '.js$')
           } else if(typeof defaultTranslationObj[engText] !== 'object') {
             defaultTranslationObj[engText] = {}
           }
-          defaultTranslationObj[engText][desc] = "TRANSLATE NEEDED"
+          defaultTranslationObj[engText][desc] = TRANSLATION_NEEDED
         }
 
       })
     }
 
-    console.log(defaultTranslationObj)
-
     // for each translation file (except English)
     fs.readdir('./src/utils/translations', (err, files) => {
       if(!err) {
         files.forEach((file, index) => {
-          if(['current.json'].includes(file)) return
+          if(['current.json', 'en.json'].includes(file)) return
           if(files.includes(file.replace(/\.json$/, '-incomplete.json'))) return
 
-          // import the json (use the [lang]-incomplete.json file if exists, otherwise [lang].json)
+          const lang = file.replace(/(?:\-incomplete)?\.json$/, '')
           const translationObj = JSON.parse(fs.readFileSync(`./src/utils/translations/${file}`, 'utf8'))
           const newTranslationObj = JSON.parse(JSON.stringify(defaultTranslationObj))
           
@@ -101,13 +103,21 @@ findInFiles.find(`i18n\\((?:[^)"']*|"[^"]*"|'[^']*')*\\)`, 'src', '.js$')
             }
           }
 
-          console.log(file, newTranslationObj)
-          // if any language variables need translation
-            // save to [lang]-incomplete.json
+          const incompleteFileContent = JSON.stringify(newTranslationObj, null, '\t')
+
+          if(incompleteFileContent.indexOf(TRANSLATION_NEEDED) !== -1) {
+            fs.writeFileSync(`./src/utils/translations/${lang}-incomplete.json`, incompleteFileContent)
+            langsGettingNewFile.push(lang)
+          } else {
+            langsNotGettingNewFile.push(lang)
+          }
         })
       } 
 
-      console.log(`Created ${numFilesCreated} translation file(s).`)
+      langsGettingNewFile.length && console.log(`Created translation file(s) for ${langsGettingNewFile.join(', ')}.`)
+      langsNotGettingNewFile.length && console.log(`Full translations already provided for ${langsNotGettingNewFile.join(', ')}.`)
+      console.log('')
+      console.log('Done.')
       console.log('')
 
       process.exit()
