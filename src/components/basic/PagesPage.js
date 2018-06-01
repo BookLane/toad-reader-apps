7438d089b972d12b919739c6d96e3d01bd0e4760
Andy Hubert <andy.hubert+8@gmail.com>
2018-06-01 10:33:23 +0300

    removed unused import

--- src/components/basic/PagesPage.js
+++ src/components/basic/PagesPage.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { StyleSheet, Platform, TouchableHighlight, TouchableNativeFeedback, Image, Dimensions, View } from "react-native"
+import { StyleSheet, Platform, TouchableHighlight, TouchableNativeFeedback, Image, View } from "react-native"
 
 import PagesBookmark from "./PagesBookmark"
 
