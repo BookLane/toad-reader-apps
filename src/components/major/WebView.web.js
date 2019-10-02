5fab1c10fc2dfd826ff11fde74b6e5a40c661990
Huberts <huberts@Hubertss-iMac.lan>
2019-10-02 10:35:25 +0300

    Untested dev on WebView component for web

--- src/components/major/WebView.web.js
+++ src/components/major/WebView.web.js
@@ -1,151 +1,181 @@
-import Qs from 'qs';
-import React, { Component } from 'react';
-import { StyleSheet, View, ActivityIndicator, createElement } from 'react-native';
-
-export default class extends Component {
-  static defaultProps = {
-    scrollEnabled: true,
-  };
-
-  state = { html: null };
-
-  constructor(props) {
-    super(props);
-    this.handleSource(props.source, props.newWindow);
-  }
-
-  setRef = ref => this.frameRef = ref;
-
-  handleSource = (source, newWindow) => {
-    if (!source.method) return;
-
-    if (newWindow) {
-      this.handleSourceInNewWindow(source, newWindow);
-    } else {
-      this.handleSourceInIFrame(source);
-    }
-  };
-
-  handleSourceInIFrame = source => {
-    const { uri, ...options } = source;
-    const baseUrl = uri.substr(0, uri.lastIndexOf('/') + 1);
-    fetch(uri, options)
-      .then(response => response.text())
-      .then(html => this.setState({ html: `<base href="${baseUrl}" />` + html }));
-  };
-
-  handleSourceInNewWindow = (source, newWindow) => {
-    if (source.method === 'POST') {
-      const contentType = source.headers['Content-Type'];
-      let body = '';
-      if (contentType && contentType.includes('application/x-www-form-urlencoded')) {
-        body = Qs.parse(source.body);
-      } else {
-        console.warn(
-          '[WebView] When opening a new window, this content-type is not supported yet, please make a PR!',
-          contentType
-        );
-        return;
-      }
-
-      window.open(
-        require('./postMock.html') +
-          '?' +
-          Qs.stringify({
-            uri: source.uri,
-            body: JSON.stringify(body),
-          }),
-        newWindow.name || 'webview',
-        newWindow.features || undefined
-      );
-    } else {
-      console.warn(
-        '[WebView] When opening a new window, this method is not supported yet, please make a PR!',
-        source.method
-      );
-    }
-  };
-
-  componentDidMount() {
-    if (this.props.onMessage) {
-      window.addEventListener('message', this.onMessage, true);
-    }
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (
-      this.props.source.uri !== nextProps.source.uri ||
-      this.props.source.method !== nextProps.source.method ||
-      this.props.source.body !== nextProps.source.body
-    ) {
-      this.handleSource(nextProps.source, nextProps.newWindow);
-    }
-  }
-
-  componentWillUnmount() {
-    if (this.props.onMessage) {
-      window.removeEventListener('message', this.onMessage, true);
-    }
-  }
-
-  onMessage = nativeEvent => this.props.onMessage({ nativeEvent });
-
-  postMessage = (message, origin) => {
-    this.frameRef.contentWindow.postMessage(message, origin);
-  };
-
-  handleInjectedJavaScript = html => {
-    if (this.props.injectedJavaScript) {
-      if (html) {
-        return html.replace('</body>', `<script>${this.props.injectedJavaScript}</script></body>`);
-      } else {
-        return html;
-      }
-    } else {
-      return html;
-    }
-  };
-
-  render() {
-    if (this.props.newWindow) {
-      return (
-        <View style={styles.loadingContainer}>
-          <ActivityIndicator />
-        </View>
-      );
-    }
-
-    const { title, source, onLoad, scrollEnabled } = this.props;
-    const styleObj = StyleSheet.flatten(this.props.style);
-    return createElement('iframe', {
-      title,
-      ref: this.setRef,
-      src: !source.method ? source.uri : undefined,
-      srcDoc: this.handleInjectedJavaScript(this.state.html || source.html),
-      width: styleObj && styleObj.width,
-      height: styleObj && styleObj.height,
-      style: [styles.iframe, scrollEnabled && styles.noScroll],
-      allowFullScreen: true,
-      allowpaymentrequest: 'true',
-      frameBorder: '0',
-      seamless: true,
-      onLoad,
-    });
-  }
-}
+import React, { useEffect } from 'react'
+import { StyleSheet, ActivityIndicator } from 'react-native'
 
 const styles = StyleSheet.create({
-  loadingContainer: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
   iframe: {
     width: '100%',
     height: '100%',
     borderWidth: 0,
   },
-  noScroll: {
-    overflow: 'hidden',
-  },
-});
+})
+
+const WebView = ({
+  source={},  // Need to support uri and headers keys
+  ref,
+  style,
+  injectedJavaScript,  // Need custom code in readium-js-viewer
+  onLoad,
+  onMessage,  // Need custom code in readium-js-viewer
+  onNavigationStateChange,  // Need custom code in readium-js-viewer
+  onError,  // Need custom code in readium-js-viewer
+  // originWhitelist,
+  // mixedContentMode,
+  // bounces,
+  // allowFileAccess,
+  // allowUniversalAccessFromFileURLs,
+  // automaticallyAdjustContentInsets,
+  // mediaPlaybackRequiresUserAction,
+  // nativeConfig,
+  // onLoadEnd,
+  // onLoadStart,
+  // onLoadProgress,
+  // onHttpError,
+  // onContentProcessDidTerminate,
+  // renderError,
+  // renderLoading,
+  // scalesPageToFit,
+  // onShouldStartLoadWithRequest,
+  // startInLoadingState,
+  // decelerationRate,
+  // domStorageEnabled,
+  // javaScriptEnabled,
+  // androidHardwareAccelerationDisabled,
+  // thirdPartyCookiesEnabled,
+  // userAgent,
+  // applicationNameForUserAgent,
+  // allowsFullscreenVideo,
+  // allowsInlineMediaPlayback,
+  // overScrollMode,
+  // contentInset,
+  // contentInsetAdjustmentBehavior,
+  // dataDetectorTypes,
+  // scrollEnabled,
+  // directionalLockEnabled,
+  // geolocationEnabled,
+  // allowingReadAccessToURL,
+  // url,
+  // html,
+  // keyboardDisplayRequiresUserAction,
+  // hideKeyboardAccessoryView,
+  // allowsBackForwardNavigationGestures,
+  // incognito,
+  // saveFormDataDisabled,
+  // cacheEnabled,
+  // pagingEnabled,
+  // allowsLinkPreview,
+  // sharedCookiesEnabled,
+  // textZoom,
+}) => {
+
+  const [ srcDoc, setSrcDoc ] = useState(null)
+  const frameRef = useRef()
+
+  useEffect(
+    () => {
+      const { uri, ...options } = source
+      const baseUrl = uri.substr(0, uri.lastIndexOf('/') + 1)
+      fetch(uri, options)
+        .then(({ ok, status, text }) => {
+          if(!ok) throw new Error(`Fetch failed with status ${status}.`)
+          return text()
+        })
+        .then(html => {
+
+          const setupJS = ``
+
+          html = `<base href="${baseUrl}" />${html}`
+          html = html.replace(/<\/body *>/i, `<script>${setupJS}</script><script>${injectedJavaScript || ``}</script></body>`)
+
+          setSrcDoc(html)
+
+        })
+        .catch(err => {
+          onError && onError({
+            code: err.name,
+            description: err.message,
+            nativeEvent: err,
+          })
+        })
+    },
+    [ source ],
+  )
+
+  useEffect(
+    () => {
+
+      const onMessageWrapper = nativeEvent => {
+        // check that it is the right iframe sending the postmessage
+
+        console.log('nativeEvent', nativeEvent)
+
+
+        // could be for: onMessage, onNavigationStateChange, onLoad
+
+
+        // onMessage({ nativeEvent })
+      }
+
+      window.addEventListener('message', onMessageWrapper, true)
+      return window.removeEventListener('message', onMessageWrapper, true)
+    },
+    [],
+  )
+
+  useEffect(
+    () => {
+
+      const methods = {
+
+        reload: () => {
+          frameRef.contentWindow.location.reload()
+        },
+
+        injectJavaScript: jsStr => {
+          frameRef.contentWindow.postMessage(
+            {
+              action: `injectJS`,
+              jsStr,
+            },
+            source.uri.replace(/^(https?:\/\/[^\/]*).*$/, '$1')
+          )
+
+        },
+
+        // Other methods:
+        //   extraNativeComponentConfig
+        //   goForward
+        //   goBack
+        //   stopLoading
+
+      }
+
+      if(typeof ref === 'function') {
+        ref(methods)
+      } else if(typeof ref === 'object') {
+        ref.current = methods
+      }
+
+    },
+    [ ref ],
+  )
+
+  if(!srcDoc) {
+    return (
+      <ActivityIndicator />
+    )
+  }
+
+  return (
+    <iframe
+      ref={frameRef}
+      src={source.uri}
+      srcDoc={srcDoc}
+      style={[
+        styles.iframe,
+        style,
+      ]}
+    />
+  )
+}
+
+export default WebView
