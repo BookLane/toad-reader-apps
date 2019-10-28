74ae27a63fbf50a70e1491d36b060edd4863fe8d
Huberts <huberts@Hubertss-iMac.lan>
2019-10-28 11:12:28 +0200

    Added missing import

--- src/components/basic/ProgressDotLabel.js
+++ src/components/basic/ProgressDotLabel.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react"
+import React, { useState, useEffect } from "react"
 import { StyleSheet, Platform, Text } from "react-native"
 import i18n from "../../utils/i18n.js"
 import useSetTimeout from '../../hooks/useSetTimeout'
