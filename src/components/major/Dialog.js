fa9eccaaeeeef551d6b27625299b7d6b978d7de8
Huberts <huberts@Hubertss-iMac.lan>
2019-11-21 13:54:07 +0200

    Removed temporary hack given centering bug in Modal

--- src/components/major/Dialog.js
+++ src/components/major/Dialog.js
@@ -130,13 +130,9 @@ const Dialog = ({
     }
   }
 
-  // See my issue here: https://github.com/akveo/react-native-ui-kitten/issues/718
-  // If they do not fix it, use ModalService, based off their Modal code.
-
   return (
     <Modal
       visible={!!open}
-      style={{ left: 50, top: 50 }}  // This is temporary, until ui-kitten fixes the issue I reported (https://github.com/akveo/react-native-ui-kitten/issues/699)
       allowBackdrop={true}
       backdropStyle={styles.modalBackdrop}
     >
