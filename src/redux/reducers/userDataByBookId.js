e84378f288356181923dc2c4d8d9aae749400e33
Huberts <huberts@Hubertss-iMac.lan>
2019-11-21 15:41:47 +0200

    Updated SET_USER_DATA to retain classrooms saved offline

--- src/redux/reducers/userDataByBookId.js
+++ src/redux/reducers/userDataByBookId.js
@@ -10,12 +10,15 @@ export default function(state = initialState, action) {
   let classrooms = [ ...(userDataForThisBook.classrooms || []) ]
   const now = Date.now()
 
+  const retainFilter = items => items.filter(({ updated_at, _delete }) => updated_at > action.lastSuccessfulPatch && !_delete)
+
   switch (action.type) {
 
-    case "SET_USER_DATA":
+    case "SET_USER_DATA": {
       // It is possible that a new edit was made since the refreshUserData was called, so I need to
       // retain anything newer than lastSuccessfulPatch
-      const highlightsToRetain = highlights.filter(highlight => highlight.updated_at > action.lastSuccessfulPatch && !highlight._delete)
+
+      const highlightsToRetain = retainFilter(highlights)
       const partialHighlightsFromTheRefresh = action.userData.highlights.filter(highlight => (
         !highlightsToRetain.some(localHighlight => (
           localHighlight.spineIdRef === highlight.spineIdRef
@@ -23,20 +26,93 @@ export default function(state = initialState, action) {
         ))
       ))
 
+      // insert in classrooms
+      let newClassrooms = [ ...(action.userData.classrooms || []) ]
+      retainFilter(classrooms).forEach(classroom => {
+        let foundClassroom = false
+        newClassrooms = newClassrooms.map(newClassroom => {
+          if(classroom.uid === newClassrooms.uid) {
+            foundClassroom = true
+            return {
+              ...classroom,
+              members: newClassrooms.members,
+              tools: newClassrooms.tools,
+            }
+          }
+          return newClassroom
+        })
+        if(!foundClassroom) {
+          newClassrooms.push(classroom)
+        }
+      })
+
+      // insert in members and tools
+      classrooms.forEach(({ uid, members={}, tools={} }) => {
+        const membersToRetain = retainFilter(members)
+        const toolsToRetain = retainFilter(tools)
+
+        if(membersToRetain.length + toolsToRetain.length === 0) return
+
+        newClassrooms = newClassrooms.map(newClassroom => {
+          if(uid === newClassrooms.uid) {
+
+            let newMembers = [ ...(newClassroom.members || []) ]
+            let newTools = [ ...(newClassroom.tools || []) ]
+
+            membersToRetain.forEach(member => {
+              let foundMember = false
+              newMembers = newMembers.map(newMember => {
+                if(member.user_id === newMember.user_id) {
+                  foundMember = true
+                  return member
+                }
+                return newMember
+              })        
+              if(!foundMember) {
+                newMembers.push(member)
+              }
+            })
+
+            toolsToRetain.forEach(tool => {
+              let foundTool = false
+              newTools = newTools.map(newTool => {
+                if(tool.uid === newTool.uid) {
+                  foundTool = true
+                  return tool
+                }
+                return newTool
+              })
+              if(!foundTool) {
+                newTool.push(tool)
+              }
+            })
+
+            return {
+              ...newClassroom,
+              members: newMember,
+              tools: newTools,
+            }
+          }
+          return newClassroom
+        })
+      })
+
       newState[action.bookId] = {
         ...action.userData,
         highlights: [
           ...partialHighlightsFromTheRefresh,
           ...highlightsToRetain,
         ],
+        classrooms: newClassrooms,
       }
 
       // TODO: To set up multiple accounts properly, I need to merge all highlight sets from different
       // accounts together.
 
       return newState
+    }
 
-    case "SET_LATEST_LOCATION":
+    case "SET_LATEST_LOCATION": {
     // In the case there is no latest location data, do not update
       if(typeof action.latestLocation !== 'object' || action.latestLocation.spineIdRef == null) {
         return state
@@ -56,8 +132,9 @@ export default function(state = initialState, action) {
       }
 
       return newState
+    }
 
-    case "SET_HIGHLIGHT":
+    case "SET_HIGHLIGHT": {
       let noChange
       highlights = highlights.filter(highlight => {
         if(highlight.spineIdRef === action.spineIdRef && highlight.cfi === action.cfi) {
@@ -88,8 +165,9 @@ export default function(state = initialState, action) {
       }
 
       return newState
+    }
 
-    case "DELETE_HIGHLIGHT":
+    case "DELETE_HIGHLIGHT": {
       let highlightToDel
       highlights = highlights.filter(highlight => {
         if(highlight.spineIdRef === action.spineIdRef && highlight.cfi === action.cfi) {
@@ -115,8 +193,9 @@ export default function(state = initialState, action) {
       }
 
       return newState
+    }
 
-    case "CLEAR_USER_DATA_EXCEPT_PROGRESS":
+    case "CLEAR_USER_DATA_EXCEPT_PROGRESS": {
       let resetUserDataForThisBook = {}
 
       if(newState[action.bookId]) {
@@ -136,8 +215,9 @@ export default function(state = initialState, action) {
       newState[action.bookId] = resetUserDataForThisBook
 
       return newState
+    }
 
-    case "CREATE_CLASSROOM":
+    case "CREATE_CLASSROOM": {
       classrooms.push({
         uid: action.uid,
         name: action.name,
@@ -159,6 +239,7 @@ export default function(state = initialState, action) {
       }
 
       return newState
+    }
 
   }
 
