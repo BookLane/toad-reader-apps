327d1d97d8c174cccace1cf15bd0f7ca2e9d4e49
Huberts <huberts@Hubertss-iMac.lan>
2019-12-06 23:18:56 +0200

    Took browser support into account with useNetwork

--- src/hooks/useNetwork.web.js
+++ src/hooks/useNetwork.web.js
@@ -8,8 +8,14 @@ export const connectionInfo = {
 const useNetwork = () => {
   const newConnectionInfo = useNetworkFromReactUse()
 
-  connectionInfo.online = newConnectionInfo.online
-  connectionInfo.effectiveType = newConnectionInfo.effectiveType
+  if(typeof newConnectionInfo.online === 'boolean') {
+    connectionInfo.online = newConnectionInfo.online
+    connectionInfo.effectiveType = newConnectionInfo.effectiveType
+
+  } else {  // some browsers do not support NetworkInformation
+    connectionInfo.online = true
+    connectionInfo.effectiveType = '4g'
+  }
 
   return connectionInfo
 }
