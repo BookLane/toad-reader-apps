994029718832e6a8a4330fbdfd98cc758d1813bb
Huberts <huberts@Hubertss-iMac.lan>
2019-12-29 15:16:53 +0200

    Add instructor's introduction

--- src/components/major/InstructorsIntroduction.js
+++ src/components/major/InstructorsIntroduction.js
@@ -3,6 +3,12 @@ import { StyleSheet, View, Text } from "react-native"
 import { bindActionCreators } from "redux"
 import { connect } from "react-redux"
 
+import EditToolData from './EditToolData'
+
+import { i18n } from "inline-i18n"
+
+import useClassroomInfo from '../../hooks/useClassroomInfo'
+
 const styles = StyleSheet.create({
   container: {
   },
@@ -11,11 +17,42 @@ const styles = StyleSheet.create({
 const InstructorsIntroduction = React.memo(({
   bookId,
   inEditMode,
+  goUpdateClassroom,
 
   books,
   userDataByBookId,
 }) => {
 
+  const { accountId, classroom } = useClassroomInfo({ books, bookId, userDataByBookId })
+
+
+  if(!classroom) return null
+
+  const { uid, introduction } = classroom
+
+  if(inEditMode) {
+    const data = {}
+
+    if(introduction) {
+      data.introduction = introduction
+    }
+
+    return (
+      <EditToolData
+        classroomUid={uid}
+        accountId={accountId}
+        dataStructure={[
+          {
+            name: 'introduction',
+            type: 'text',
+            placeholder: i18n("Enter your introduction here."),
+          },
+        ]}
+        data={data}
+        goUpdateTool={goUpdateClassroom}
+      />
+    )
+  }
 
   return null
 })
