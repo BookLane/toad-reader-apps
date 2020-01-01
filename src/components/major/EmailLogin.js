82bb88211157c8dd687e0c02e7fb82183810b59b
Huberts <huberts@Hubertss-iMac.lan>
2020-01-01 16:45:49 +0200

    Login email process working

--- src/components/major/EmailLogin.js
+++ src/components/major/EmailLogin.js
@@ -5,7 +5,8 @@ import { connect } from "react-redux"
 import { withRouter } from "react-router"
 import SafeLayout from "../basic/SafeLayout"
 import { i18n } from "inline-i18n"
-import { Input, Button } from "react-native-ui-kitten"
+import Input from "../basic/Input"
+import Button from "../basic/Button"
 
 import CoverAndSpin from "../basic/CoverAndSpin"
 
@@ -15,22 +16,45 @@ import useInstanceValue from "../../hooks/useInstanceValue"
 import { addAccount } from "../../redux/actions"
 
 const styles = StyleSheet.create({
-  fullscreen: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    bottom: 0,
-    right: 0,
-    width: '100%',
-    height: '100%',
-    backgroundColor: 'white',
+  title: {
+    textAlign: 'center',
+    fontSize: 18,
+    fontWeight: 600,
+    marginTop: 40,
+    marginBottom: 20,
   },
-  hidden: {
-    transform: [
-      {
-        translateY: -99999,
-      },
-    ],
+  box: {
+    maxWidth: 300,
+    marginHorizontal: 'auto',
+    borderWidth: 1,
+    borderColor: 'rgba(0, 0, 0, 0.2)',
+    borderRadius: 3,
+    backgroundColor: 'rgba(255, 255, 255, .5)',
+    padding: 20,
+  },
+  linkContainer: {
+    marginTop: 20,
+  },
+  link: {
+    opacity: .5,
+    fontSize: 13,
+    textDecorationLine: 'underline',
+  },
+  error: {
+    marginTop: 20,
+    fontSize: 16,
+    color: 'red',
+  },
+  instructions: {
+    fontSize: 15,
+    marginBottom: 20,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+  },
+  consent: {
+    marginTop: 20,
+    fontSize: 12,
   },
 })
 
@@ -92,7 +116,7 @@ const EmailLogin = ({
 
       try {
         response = await safeFetch(
-          `${getDataOrigin(idps[idpId])}/loginwithaccesscode?code=${encodeURIComponent(getCode().trim())}`,
+          `${getDataOrigin(idps[idpId])}/loginwithaccesscode?code=${encodeURIComponent(getCode().trim().toUpperCase())}`,
           getReqOptionsWithAdditions({
             mode: 'cors',
           }),
@@ -134,56 +158,81 @@ const EmailLogin = ({
     [ idps[idpId], idpId, onSuccess ],
   )
 
+  const sendAnotherEmail = useCallback(() => setStage('AWAITING_EMAIL'), [])
+
+  const { emailConsentText, name } = idps[idpId]
+
+  const errorMessages = {
+    'invalid access code': i18n("Invalid or expired access code. Try again."),
+  }
+
   return (
     <SafeLayout>
-      {error &&
-        <Text style={styles.error}>
-          {error}
-        </Text>
-      }
-      {[ 'AWAITING_EMAIL', 'SENDING_EMAIL' ].includes(stage) &&
-        <>
-          <Input
-            label={i18n("Email")}
-            value={email}
-            onChangeText={setEmail}
-            disabled={stage === 'SENDING_EMAIL'}
-          />
-          <View style={styles.buttonContainer}>
-            <Button
-              style={styles.button}
-              status="primary"
-              textStyle={styles.buttonText}
-              onPress={goSendEmail}
-              disabled={stage === 'SENDING_EMAIL' || !isValidEmail(email.trim())}
-            >
-              {i18n("Send login code")}
-            </Button>
-          </View>
-        </>
-      }
-      {[ 'AWAITING_CODE', 'SENDING_CODE' ].includes(stage) &&
-        <>
-          <Input
-            label={i18n("Login code")}
-            placeholder={i18n("Eg. U876TV")}
-            value={code}
-            onChangeText={setCode}
-            disabled={stage === 'SENDING_CODE'}
-          />
-          <View style={styles.buttonContainer}>
-            <Button
-              style={styles.button}
-              status="primary"
-              textStyle={styles.buttonText}
-              onPress={goCheckCode}
-              disabled={stage === 'SENDING_CODE' || !code.trim()}
-            >
-              {i18n("Log in")}
-            </Button>
-          </View>
-        </>
-      }
+      <Text style={styles.title}>
+        {name}
+      </Text>
+      <View style={styles.box}>
+        {[ 'AWAITING_EMAIL', 'SENDING_EMAIL' ].includes(stage) &&
+          <>
+            <Text style={styles.instructions}>
+              {i18n("Enter your email to receive a login code.")}
+            </Text>
+            <Input
+              label={i18n("Email")}
+              value={email}
+              onChangeText={setEmail}
+              disabled={stage === 'SENDING_EMAIL'}
+            />
+            <View style={styles.buttonContainer}>
+              <Button
+                status="primary"
+                onPress={goSendEmail}
+                disabled={stage === 'SENDING_EMAIL' || !isValidEmail(email.trim())}
+              >
+                {i18n("Send login code")}
+              </Button>
+            </View>
+            {!!emailConsentText &&
+              <Text style={styles.consent}>
+                {emailConsentText}
+              </Text>
+            }
+          </>
+        }
+        {[ 'AWAITING_CODE', 'SENDING_CODE' ].includes(stage) &&
+          <>
+            <Text style={styles.instructions}>
+              {i18n("Email sent. Check your inbox and then enter the code from that email below.")}
+            </Text>
+            <Input
+              label={i18n("Login code")}
+              placeholder={i18n("Eg. U876TV")}
+              value={code}
+              onChangeText={setCode}
+              disabled={stage === 'SENDING_CODE'}
+            />
+            <View style={styles.buttonContainer}>
+              <Button
+                status="primary"
+                onPress={goCheckCode}
+                disabled={stage === 'SENDING_CODE' || !code.trim()}
+              >
+                {i18n("Log in")}
+              </Button>
+            </View>
+            <Text style={styles.linkContainer}>
+              <Text style={styles.link} onPress={sendAnotherEmail}>
+                {i18n("Send another email.")}
+              </Text>
+            </Text>
+          </>
+        }
+        {error &&
+          <Text style={styles.error}>
+            {errorMessages[error] || error}
+          </Text>
+        }
+      </View>
       {[ 'SENDING_EMAIL', 'SENDING_CODE' ].includes(stage) && !error &&
         <CoverAndSpin
           text={stage === 'SENDING_EMAIL' ? i18n("Sending email...") : i18n("Checking code...")}
