15f4e6e41519519a2aad385ac68d3174bca6b9c1
Huberts <huberts@Hubertss-iMac.lan>
2020-02-18 11:07:45 +0200

    Updated scripts: staging-to-production flow; delete old files

--- scripts/pushWebStagingToProduction.js
+++ scripts/pushWebStagingToProduction.js
@@ -8,11 +8,11 @@ const dashifyDomain = domain => domain
 const { domain } = Object.values(appInfo.expo.extra.IDPS)[0]
 
 try {
-  exec(`aws s3 sync web-build s3://${dashifyDomain(domain)}.staging.toadreader.com --acl public-read --quiet --delete`, (err, stdout, stderr) => {
+  exec(`aws s3 sync s3://${dashifyDomain(domain)}.staging.toadreader.com s3://${domain} --acl public-read --quiet --delete`, (err, stdout, stderr) => {
     console.log(stdout)
     process.exit()
   })
 } catch(err) {
-  console.log("Error when pushing web to staging.", err)
+  console.log("Error when pushing web staging to production.", err)
   process.exit()
 }
--- scripts/pushWebToProduction.js
+++ scripts/pushWebToProduction.js
@@ -1,14 +1,16 @@
-const appInfo = require('../app.json')
-const { exec } = require('child_process')
+process.exit() // use pushWebStagingToProduction.js instead
 
-const { domain } = Object.values(appInfo.expo.extra.IDPS)[0]
+// const appInfo = require('../app.json')
+// const { exec } = require('child_process')
 
-try {
-  exec(`aws s3 sync web-build s3://${domain} --acl public-read --quiet`, (err, stdout, stderr) => {
-    console.log(stdout)
-    process.exit()
-  })
-} catch(err) {
-  console.log("Error when pushing web to production.", err)
-  process.exit()
-}
+// const { domain } = Object.values(appInfo.expo.extra.IDPS)[0]
+
+// try {
+//   exec(`aws s3 sync web-build s3://${domain} --acl public-read --quiet --delete`, (err, stdout, stderr) => {
+//     console.log(stdout)
+//     process.exit()
+//   })
+// } catch(err) {
+//   console.log("Error when pushing web to production.", err)
+//   process.exit()
+// }
