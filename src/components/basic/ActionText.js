1fdc5e6f399f76cda91d8b03ae0e13b86a000e5d
Huberts <huberts@Hubertss-iMac.local>
2020-05-06 11:53:52 -0400

    Make ReadingSchedule's ActionText effectively inline-block

--- src/components/basic/ActionText.js
+++ src/components/basic/ActionText.js
@@ -1,8 +1,14 @@
 import React, { useCallback } from "react"
-import { Text, TouchableOpacity } from "react-native"
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native"
 import { styled } from "@ui-kitten/components"
 
 import useThemedStates from "../../hooks/useThemedStates"
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+  },
+})
 
 const ActionText = React.memo(({
   id,
@@ -26,18 +32,20 @@ const ActionText = React.memo(({
   )
 
   return (
-    <TouchableOpacity
-      onPress={customOnPress}
-      {...themedStateEvents}
-    >
-      <Text
-        style={[
-          themedStyle,
-          style,
-        ]}
-        {...otherProps}
-      />
-    </TouchableOpacity>
+    <View style={styles.container}>
+      <TouchableOpacity
+        onPress={customOnPress}
+        {...themedStateEvents}
+      >
+        <Text
+          style={[
+            themedStyle,
+            style,
+          ]}
+          {...otherProps}
+        />
+      </TouchableOpacity>
+    </View>
   )
 })
 
