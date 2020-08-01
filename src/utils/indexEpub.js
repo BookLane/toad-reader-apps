ed59011cfa0d706d875851813ff1e7101b0ccdd9
Huberts <huberts@Hubertss-iMac.local>
2020-07-31 22:35:14 -0400

    Added missing import

--- src/utils/indexEpub.js
+++ src/utils/indexEpub.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Text } from "react-native"
+import { Text, Platform } from "react-native"
 import MiniSearch from "minisearch"
 import * as FileSystem from 'expo-file-system'
 
