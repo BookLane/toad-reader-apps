09c53cccf74954949aac9fe03a42beb7e167b2d1
Huberts <huberts@Hubertss-iMac.local>
2020-08-14 09:43:51 -0400

    encode data domains instead of dashifying

--- src/utils/toolbox.js
+++ src/utils/toolbox.js
@@ -285,6 +285,74 @@ export const dashifyDomain = domain => domain
   .replace(/-/g, '--')
   .replace(/\./g, '-')
 
+const convertBase = ({ str, fromBase, toBase }) => {
+  // Based off an answer here: https://stackoverflow.com/questions/1337419/how-do-you-convert-numbers-between-different-bases-in-javascript
+  // Needed because (1) javascript only does up to base 36, and (2) I needed to customize the digits to what is below.
+
+  const DIGITS = "0123456789abcdefghijklmnopqrstuvwxyz-.";
+
+  const add = (x, y, base) => {
+      let z = [];
+      const n = Math.max(x.length, y.length);
+      let carry = 0;
+      let i = 0;
+      while (i < n || carry) {
+          const xi = i < x.length ? x[i] : 0;
+          const yi = i < y.length ? y[i] : 0;
+          const zi = carry + xi + yi;
+          z.push(zi % base);
+          carry = Math.floor(zi / base);
+          i++;
+      }
+      return z;
+  }
+
+  const multiplyByNumber = (num, x, base) => {
+      if (num < 0) return null;
+      if (num == 0) return [];
+
+      let result = [];
+      let power = x;
+      while (true) {
+          num & 1 && (result = add(result, power, base));
+          num = num >> 1;
+          if (num === 0) break;
+          power = add(power, power, base);
+      }
+
+      return result;
+  }
+
+  const parseToDigitsArray = (str, base) => {
+      const digits = str.split('');
+      let arr = [];
+      for (let i = digits.length - 1; i >= 0; i--) {
+          const n = DIGITS.indexOf(digits[i])
+          if (n == -1) return null;
+          arr.push(n);
+      }
+      return arr;
+  }
+
+  const digits = parseToDigitsArray(str, fromBase);
+  if (digits === null) return null;
+
+  let outArray = [];
+  let power = [1];
+  for (let i = 0; i < digits.length; i++) {
+      digits[i] && (outArray = add(outArray, multiplyByNumber(digits[i], power, toBase), toBase));
+      power = multiplyByNumber(fromBase, power, toBase);
+  }
+
+  let out = '';
+  for (let i = outArray.length - 1; i >= 0; i--)
+      out += DIGITS[outArray[i]];
+
+  return out;
+}
+
+export const encodeDomain = domain => convertBase({ str: domain, fromBase: 38, toBase: 36 })
+
 export const isStaging = () => (
   Constants.manifest.releaseChannel === 'staging'
   || (
@@ -310,11 +378,11 @@ export const getDataOrigin = ({ domain, protocol=`https` }={}) => {
 
   if(isStaging()) {
     // staging environment
-    return `${protocol}://${dashifyDomain(domain)}.data.staging.toadreader.com`
+    return `${protocol}://${encodeDomain(domain)}.data.staging.toadreader.com`
   }
 
   // production or beta environment
-  return `${protocol}://${dashifyDomain(domain)}.data.toadreader.com`
+  return `${protocol}://${encodeDomain(domain)}.data.toadreader.com`
 
 }
 
