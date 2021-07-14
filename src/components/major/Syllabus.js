340acd7e84b1aff270f2dc71cfb68c4f301648c3
AndyHubert <andy.hubert@gmail.com>
2021-07-14 16:02:42 -0400

    Added note about including a syllabus

--- src/components/major/Syllabus.js
+++ src/components/major/Syllabus.js
@@ -1,19 +1,33 @@
 import React from "react"
-import { StyleSheet, ScrollView } from "react-native"
+import { StyleSheet, ScrollView, Text } from "react-native"
 import { bindActionCreators } from "redux"
 import { connect } from "react-redux"
 import { i18n } from "inline-i18n"
 
 import useClassroomInfo from '../../hooks/useClassroomInfo'
+import useWideMode from "../../hooks/useWideMode"
 import useChangeIndex from '../../hooks/useChangeIndex'
 import useAssetBaseUri from "../../hooks/useAssetBaseUri"
 
 import EditToolData from './EditToolData'
 import Document from './Document'
 
+const instructions = {
+  margin: 20,
+  marginBottom: 20,
+  fontWeight: '200',
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  instructions: {
+    ...instructions,
+  },
+  instructionsWideMode: {
+    ...instructions,
+    marginHorizontal: 30,
   },
 })
 
@@ -29,6 +43,8 @@ const Syllabus = React.memo(({
   books,
   userDataByBookId,
 }) => {
+
+  const wideMode = useWideMode()
 
   const { accountId, classroom, idpId, hasFrontMatterDraftData } = useClassroomInfo({ books, bookId, userDataByBookId })
   const { uid, syllabus, draftData } = classroom || {}
@@ -55,6 +71,11 @@ const Syllabus = React.memo(({
   if(inEditMode && !viewingPreview) {
     return (
       <ScrollView style={styles.container}>
+        {!data.syllabus &&
+          <Text style={wideMode ? styles.instructionsWideMode : styles.instructions}>
+            {i18n("Include your course syllabus here to make it easy for students to find.", "", "enhanced")}
+          </Text>
+        }
         <EditToolData
           key={changeIndex}
           classroomUid={uid}
