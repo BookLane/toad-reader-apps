65e2f7857067d48ae822f6480db4091e6c65816c
AndyHubert <andy.hubert@gmail.com>
2023-11-08 14:47:46 -0500

    audiobook: Better looking chapter button

--- src/components/major/AudiobookPlayerChapterLine.js
+++ src/components/major/AudiobookPlayerChapterLine.js
@@ -1,7 +1,9 @@
-import React from "react"
+import React, { useCallback } from "react"
 import { StyleSheet, View, Text } from "react-native"
-import HeaderIcon from "../basic/HeaderIcon"
 // import { i18n } from "inline-i18n"
+
+import Icon from "../basic/Icon"
+import Button from "../basic/Button"
 
 const styles = StyleSheet.create({
   chapter: {
@@ -14,8 +16,17 @@ const styles = StyleSheet.create({
   chapterText: {
     fontSize: 15,
   },
-  chapterIcon: {
+  button: {
     marginLeft: 10,
+    borderRadius: 25,
+    height: 34,
+    width: 34,
+    maxWidth: 34,
+    paddingHorizontal: 0,
+    paddingVertical: 0,
+  },
+  icon: {
+    height: 18,
   },
 })
 
@@ -23,17 +34,19 @@ const AudiobookPlayerChapterLine = ({
   label,
 }) => {
 
+  const ChaptersIcon = useCallback(({ style }) => <Icon name='text' pack="materialCommunity" style={styles.icon} />, [])
+
   return (
     <View style={styles.chapter}>
       <Text style={styles.chapterText}>
         {label}
       </Text>
-      <HeaderIcon
-        iconName="text"
-        iconPack="materialCommunity"
-        // onPress={}
-        uiStatus="faded"
-        style={styles.chapterIcon}
+      <Button
+        style={styles.button}
+        status="basic"
+        appearance="ghost"
+        accessoryLeft={ChaptersIcon}
+        // onPress={backTen}
       />
     </View>
   )
