1bada83eced3bdaba43ee73a33b4803279f6c5a2
AndyHubert <andy.hubert@gmail.com>
2024-02-14 09:47:57 -0500

    Improved email page

--- src/components/major/EmailLogin.js
+++ src/components/major/EmailLogin.js
@@ -4,56 +4,107 @@ import { bindActionCreators } from "redux"
 import { connect } from "react-redux"
 import SafeLayout from "../basic/SafeLayout"
 import { i18n } from "inline-i18n"
-import Input from "../basic/Input"
-import Button from "../basic/Button"
-
-import CoverAndSpin from "../basic/CoverAndSpin"
+import { Image } from 'expo-image'
+import Constants from 'expo-constants'
 
 import { safeFetch, getReqOptionsWithAdditions, getDataOrigin, isValidEmail } from "../../utils/toolbox"
 import useInstanceValue from "../../hooks/useInstanceValue"
 import useRouterState from "../../hooks/useRouterState"
 
+import CoverAndSpin from "../basic/CoverAndSpin"
+import Input from "../basic/Input"
+import Button from "../basic/Button"
+
 import { addAccount } from "../../redux/actions"
+
+const {
+  HAS_LOGIN_LOGO=false,
+  LOGIN_SCREEN_BG_COLOR="white",
+} = Constants.expoConfig.extra
 
 const styles = StyleSheet.create({
   background: {
     flex: 1,
     alignItems: 'center',
+    backgroundColor: LOGIN_SCREEN_BG_COLOR,
+  },
+  coverAndSpin: {
+    backgroundColor: LOGIN_SCREEN_BG_COLOR,
+  },
+  titleContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 5,
+    marginTop: 120,
   },
   title: {
-    textAlign: 'center',
-    fontSize: 18,
+    fontSize: 20,
     fontWeight: '600',
-    marginTop: 40,
-    marginBottom: 20,
+  },
+  titleLogoImage: {
+    width: 40,
+    height: 40,
+    resizeMode: 'cover',
+  },
+  titleImage: {
+    width: 300,
+    height: 159,
+    marginTop: 120,
+    resizeMode: 'cover',
   },
   box: {
-    maxWidth: 300,
-    borderWidth: 1,
-    borderColor: 'rgba(0, 0, 0, 0.2)',
-    borderRadius: 3,
-    backgroundColor: 'rgba(51, 102, 255, .1)',
-    padding: 20,
+    marginTop: 30,
+    width: 300,
   },
   linkContainer: {
-    marginTop: 20,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
   },
   link: {
     opacity: .5,
     fontSize: 13,
     textDecorationLine: 'underline',
   },
+  warning: {
+    marginTop: 20,
+    fontSize: 14,
+    color: 'red',
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: 'rgba(0, 0, 0, .1)',
+    padding: 15,
+  },
   error: {
     marginTop: 20,
     fontSize: 15,
     color: 'red',
+    textAlign: 'center',
   },
   instructions: {
     fontSize: 15,
     marginBottom: 20,
+    textAlign: 'center',
+    fontWeight: '500',
   },
   buttonContainer: {
+    marginVertical: 10,
     flexDirection: 'row',
+  },
+  button: {
+    width: '100%',
+    backgroundColor: 'rgba(0, 0, 0, .95)',
+    borderColor: 'rgba(0, 0, 0, .95)',
+  },
+  buttonDisabled: {
+    backgroundColor: 'rgba(0, 0, 0, .3)',
+    borderColor: 'transparent',
+  },
+  inputLabel: {
+    fontSize: 13,
+    color: 'rgba(0, 0, 0, .95)',
   },
   consent: {
     marginTop: 20,
@@ -177,7 +228,14 @@ const EmailLogin = ({
     [],
   )
 
-  const sendAnotherEmail = useCallback(() => setStage('AWAITING_EMAIL'), [])
+  const sendAnotherEmail = useCallback(
+    () => {
+      setStage('AWAITING_EMAIL')
+      setError()
+      setNumSessionsThisWillLogOut(0)
+    },
+    [],
+  )
 
   const { emailConsentText, name } = idps[idpId]
 
@@ -185,20 +243,35 @@ const EmailLogin = ({
     'invalid access code': i18n("Invalid or expired access code. Try again."),
   }
 
+  const stage1Disabled = stage === 'SENDING_EMAIL' || !isValidEmail(email.trim())
+  const stage2Disabled = stage === 'SENDING_CODE' || !code.trim()
+
   return (
     <SafeLayout>
       <View style={styles.background}>
-        <Text style={styles.title}>
-          {name}
-        </Text>
+        {!HAS_LOGIN_LOGO &&
+          <View style={styles.titleContainer}>
+            <Image
+              source={require("../../../assets/icons/logo.png")}
+              style={styles.titleLogoImage}
+            />
+            <Text style={styles.title}>
+              {name}
+            </Text>
+          </View>
+        }
+        {HAS_LOGIN_LOGO &&
+          <Image
+            source={require("../../../assets/icons/login-logo.png")}
+            style={styles.titleImage}
+          />
+        }
         <View style={styles.box}>
           {[ 'AWAITING_EMAIL', 'SENDING_EMAIL' ].includes(stage) &&
             <>
-              <Text style={styles.instructions}>
-                {i18n("Enter your email to receive a login code.")}
-              </Text>
               <Input
                 label={i18n("Email")}
+                placeholder={i18n("Email address")}
                 value={email}
                 name="email"
                 onChangeText={setEmail}
@@ -209,16 +282,25 @@ const EmailLogin = ({
                 autoCompleteType="email"
                 keyboardType="email-address"
                 textContentType="emailAddress"
+                size='small'
+                labelStyle={styles.inputLabel}
               />
               <View style={styles.buttonContainer}>
                 <Button
                   status="primary"
-                  onPress={goSendEmail}
-                  disabled={stage === 'SENDING_EMAIL' || !isValidEmail(email.trim())}
+                  style={[
+                    styles.button,
+                    (stage1Disabled && styles.buttonDisabled),
+                  ]}
+                  size="small"
+                  onPress={stage1Disabled ? null : goSendEmail}
                 >
-                  {i18n("Send login code")}
+                  {i18n("Continue with email")}
                 </Button>
               </View>
+              <Text style={styles.instructions}>
+                {i18n("Enter your email to receive a login code.")}
+              </Text>
               {!!emailConsentText &&
                 <Text style={styles.consent}>
                   {emailConsentText}
@@ -228,9 +310,6 @@ const EmailLogin = ({
           }
           {[ 'AWAITING_CODE', 'SENDING_CODE' ].includes(stage) &&
             <>
-              <Text style={styles.instructions}>
-                {i18n("Email sent. Check your inbox and enter the code from that email.")}
-              </Text>
               <Input
                 label={i18n("Login code")}
                 placeholder={i18n("Eg. {{example}}", { example: "U876TV" })}
@@ -241,16 +320,25 @@ const EmailLogin = ({
                 autoCapitalize="characters"
                 autoFocus={true}
                 autoCompleteType="off"
+                labelStyle={styles.inputLabel}
+                size='small'
               />
               <View style={styles.buttonContainer}>
                 <Button
                   status="primary"
-                  onPress={goCheckCode}
-                  disabled={stage === 'SENDING_CODE' || !code.trim()}
+                  style={[
+                    styles.button,
+                    (stage2Disabled && styles.buttonDisabled),
+                  ]}
+                  size="small"
+                  onPress={stage2Disabled ? null : goCheckCode}
                 >
                   {i18n("Log in")}
                 </Button>
               </View>
+              <Text style={styles.instructions}>
+                {i18n("Email sent. Check your inbox and enter the code from that email.")}
+              </Text>
               <Text style={styles.linkContainer}>
                 <Text style={styles.link} onPress={sendAnotherEmail}>
                   {i18n("Send another email")}
@@ -259,7 +347,7 @@ const EmailLogin = ({
             </>
           }
           {numSessionsThisWillLogOut > 0 &&
-            <Text style={styles.error}>
+            <Text style={styles.warning}>
               {i18n("Warning: You are already logged into the maximum of number of devices ({{num}} devices). Continuing to log in here will log you out elsewhere.", { num: numSessionsThisWillLogOut })}
             </Text>
           }
@@ -272,7 +360,7 @@ const EmailLogin = ({
         {[ 'SENDING_EMAIL', 'SENDING_CODE' ].includes(stage) && !error &&
           <CoverAndSpin
             text={stage === 'SENDING_EMAIL' ? i18n("Sending email...") : i18n("Checking code...")}
-            style={{ backgroundColor: 'white' }}
+            style={styles.coverAndSpin}
           />
         }
       </View>
