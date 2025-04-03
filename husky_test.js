557dadcc367ccbe2b6cb1d3e6e7c207d39bad0c0
alitokgonul <alitokgonul@gmail.com>
2025-04-03 12:03:40 +0300

    Revert "integrate lint and husky"
    This reverts commit 09e81b444cb741af51567337d8058cd3b4260d70.

---
+++ husky_test.js
@@ -0,0 +1,13 @@
+const message = 'Hello Husky';
+
+function sayHi() {
+  console.log(message);
+}
+sayHi();
+
+const message1 = 'Hello Husky';
+
+function sayHi1() {
+  console.log(message1);
+}
+sayHi1();
