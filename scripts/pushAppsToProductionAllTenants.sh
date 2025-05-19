04ceb443c22b8aae118fe69b59b71166de5c352e
Trevor Dearham <trevor.dearham@gmail.com>
2025-05-19 10:15:36 +0000

    Remove external tenants

--- scripts/pushAppsToProductionAllTenants.sh
+++ scripts/pushAppsToProductionAllTenants.sh
@@ -1,31 +1,10 @@
 #!/bin/bash
 
-npm run change-tenant -- toadreader
-npm run go-push-apps-to-production
-
 npm run change-tenant -- biblemesh
 npm run go-push-apps-to-production
 
 npm run change-tenant -- zondervan
 npm run go-push-apps-to-production
 
-# npm run change-tenant -- g12
-# npm run go-push-apps-to-production
-
-# npm run change-tenant -- proyectonehemias
-# npm run go-push-apps-to-production
-
-npm run change-tenant -- homeschool
-npm run go-push-apps-to-production
-
-# npm run change-tenant -- readyman
-# npm run go-push-apps-to-production
-
 npm run change-tenant -- langham
-npm run go-push-apps-to-production
-
-npm run change-tenant -- blf
-npm run go-push-apps-to-production
-
-npm run change-tenant -- kmk
 npm run go-push-apps-to-production
--- scripts/pushWebToProductionAllTenants.sh
+++ scripts/pushWebToProductionAllTenants.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-npm run change-tenant -- toadreader
-npm run go-push-web-to-staging
-node ./scripts/pushwebstagingtoproduction.js
-
 npm run change-tenant -- biblemesh
 npm run go-push-web-to-staging
 node ./scripts/pushwebstagingtoproduction.js
@@ -12,38 +8,6 @@ npm run change-tenant -- zondervan
 npm run go-push-web-to-staging
 node ./scripts/pushwebstagingtoproduction.js
 
-# npm run change-tenant -- g12
-# npm run go-push-web-to-staging
-# node ./scripts/pushwebstagingtoproduction.js
-
-# npm run change-tenant -- proyectonehemias
-# npm run go-push-web-to-staging
-# node ./scripts/pushwebstagingtoproduction.js
-
-npm run change-tenant -- homeschool
-npm run go-push-web-to-staging
-node ./scripts/pushwebstagingtoproduction.js
-
-# npm run change-tenant -- readyman
-# npm run go-push-web-to-staging
-# node ./scripts/pushwebstagingtoproduction.js
-
-npm run change-tenant -- biblearc
-npm run go-push-web-to-staging
-node ./scripts/pushwebstagingtoproduction.js
-
-# npm run change-tenant -- bridge
-# npm run go-push-web-to-staging
-# node ./scripts/pushwebstagingtoproduction.js
-
 npm run change-tenant -- langham
-npm run go-push-web-to-staging
-node ./scripts/pushwebstagingtoproduction.js
-
-npm run change-tenant -- blf
-npm run go-push-web-to-staging
-node ./scripts/pushwebstagingtoproduction.js
-
-npm run change-tenant -- kmk
 npm run go-push-web-to-staging
 node ./scripts/pushwebstagingtoproduction.js
