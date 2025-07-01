de9c355cb029112a48ee48302a74f659b9791d2e
Trevor Dearham <trevor.dearham@gmail.com>
2025-07-01 11:39:23 +0000

    Don't suppress error and warning messages

--- scripts/rollBackWeb.js
+++ scripts/rollBackWeb.js
@@ -18,7 +18,7 @@ const versionBucket = appInfo.expo.extra.VERSION_BUCKET
 
 try {
   const bucketProduction = `${bucketPrefix}-prod`.slice(0,63)
-  executeCommand(`aws s3 cp s3://${versionBucket}/${domain}/${date} s3://${bucketProduction} --recursive --sse AES256 --quiet`, () => {
+  executeCommand(`aws s3 cp s3://${versionBucket}/${domain}/${date} s3://${bucketProduction} --only-show-errors --recursive --sse AES256`, () => {
     process.exit()
   })
 } catch(err) {
